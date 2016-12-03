2d774936afcb7ac9e646f28e3132bcea5af33b53
Kevin Bottoms <kevinbottoms13@gmail.com>
2016-12-03 01:34:01 +0100

    Add perfect matches to user show on scaffolded user view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+
+    @users = User.where("myskills = ? AND wantedskills = ?", @user.wantedskills, @user.myskills)
   end
 
   # GET /users/new
